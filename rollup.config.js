0d5ae29496424bca14b0970156a3c105dea97290
Jaesung Park <jaesung.cs@gmail.com>
2023-08-22 20:14:31 +0900

    typescript, importmap

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
+import typescript from '@rollup/plugin-typescript';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 
 const watch = process.env.ROLLUP_WATCH;
 
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: {
         file: 'dist/bundle.js',
     },
     plugins: [
+        typescript(),
         watch && serve(),
         watch && livereload(),
     ],
